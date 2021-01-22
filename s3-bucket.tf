1d0299e03341e1cc415a2fe6e14ba8218ad7a53d
root <mihai.stanciu@me.com>
2021-01-22 09:27:46 +0000

    adding s3 bucket

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -3,10 +3,5 @@
 module "s3_bucket" {
   source  = "app.terraform.io/TechDev-training/s3-bucket/aws"
   version = "1.15.0"
-
-  acceleration_status = "null"
-  bucket = "null"
   bucket_prefix = "mihai-stanciu"
-  policy = "null"
-  request_payer = "null"
 }
